048d7f5d95ccb98881fd79e30ff99cd2a9a1e887
David Balogh <david.balogh@cae.com>
2022-04-18 19:06:37 +0200

    heroku server

--- test_server.py
+++ test_server.py
@@ -4,5 +4,5 @@ import json
 if __name__ == "__main__":
     with open('./test_payload.json','r') as fp:
         data=json.load(fp)
-        result = requests.post('http://127.0.0.1:8000/guess', json=data)
+        result = requests.post('https://polar-fortress-90021.herokuapp.com/guess', json=data)
         print(f"==[ result: {result.json()}")
