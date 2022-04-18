b94d771d148176c672a269b3f9ee4d29e361f946
David Balogh <david.balogh@cae.com>
2022-04-18 12:15:54 +0200

    simplify server test file

--- test_server.py
+++ test_server.py
@@ -1,19 +1,8 @@
 import requests
 import json
 
-# from test_payload import payload
-payload=[
-    ["a", 1, "b", 2, "c", 3, "d", 3, "e", 1],
-    ["a", 1, "b", 2, "c", 3, "d", 3, "e", 1],
-    ["a", 1, "b", 2, "c", 3, "d", 3, "e", 1],
-    ["a", 1, "b", 2, "c", 3, "d", 3, "e", 1],
-    ["a", 1, "b", 2, "c", 3, "d", 3, "e", 1],
-    ["a", 1, "b", 2, "c", 3, "d", 3, "e", 1],
-]
 if __name__ == "__main__":
     with open('./test_payload.json','r') as fp:
         data=json.load(fp)
-        # print(data)
         result = requests.post('http://127.0.0.1:8000/guess', json=data)
-        # print(result)
         print(f"==[ result: {result.json()}")
