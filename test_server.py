168ca696c45582f73e092c6f9ef8bbba83fb19a8
David Balogh <david.balogh@cae.com>
2022-04-19 18:16:53 +0200

    update Heroku domain name

--- test_server.py
+++ test_server.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     with open("./test_payload.json", "r") as fp:
         data = json.load(fp)
         result = requests.post(
-            "https://polar-fortress-90021.herokuapp.com/guess", json=data
+            "https://szozat-wordle-solver.herokuapp.com/guess", json=data
         )
         print(f"==[ result: {result.json()}")
